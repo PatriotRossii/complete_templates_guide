e02b68dcd435b989570192ff93969dee312417ba
PatriotRossii <patriotrossii2019@mail.ru>
2021-07-09 10:42:58 +0000

    Add maxdecltype def and usage example

---
+++ basics/maxdecltype_usage.cpp
@@ -0,0 +1,7 @@
+#include "maxdecltype.hpp"
+#include <iostream>
+
+int main() {
+    std::cout << "max(1, 2.1): " << ::max(1, 2.1) << '\n';
+    std::cout << "max(1.5, 99): " << ::max(1.5, 99) << '\n';
+}
